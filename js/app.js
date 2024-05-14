aa1631befdb1c693c489068ba225917810413179
limitlesscreator <0428vlad0428@mail.ru>
2024-05-14 10:31:44 +0300

    test

--- js/app.js
+++ js/app.js
@@ -54,3 +54,62 @@ const sendMail = () => {
 }
 
 document.querySelector('.bookARideBtn').addEventListener('click',() => sendMail())
+
+
+
+//удалить
+
+  document.querySelector('.closeMainModal').click()
+
+
+
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = easing(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function easing(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 *t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+var home = document.querySelector('#home');
+var about = document.querySelector('#about');
+var services = document.querySelector('#services');
+var contact = document.querySelector('#contact');
+
+var homeLink = document.querySelector('a[href="#home"]');
+var aboutLink = document.querySelector('a[href="#about"]');
+var servicesLink = document.querySelector('a[href="#services"]');
+var contactLink = document.querySelector('a[href="#contact"]');
+
+homeLink.addEventListener('click', function() {
+  smoothScroll('#home', 1000);
+});
+
+aboutLink.addEventListener('click', function() {
+  smoothScroll('#about', 1000);
+});
+
+servicesLink.addEventListener('click', function() {
+  smoothScroll('#services', 1000);
+});
+
+contactLink.addEventListener('click', function() {
+  smoothScroll('#contact', 1000);
+});
